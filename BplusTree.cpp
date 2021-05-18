dbb2471bed15709e6895319f561dea9fa3e762e5
Sam-Chu-07 <73769352+Sam-Chu-07@users.noreply.github.com>
2021-05-19 00:50:41 +0800

    version 5/19 00:50

--- BplusTree.cpp
+++ BplusTree.cpp
@@ -1,82 +1,153 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <string>
 using namespace std;
 
 
-
+// --- B+ Tree --- //
 class Btree
 {
     public:
         Btree(int input_order);
 
+        Insert(int value);
 
 
-    // ---- B+ Tree pivate ---- // 
+
+    
+
+    // --- Private 部分 --- //
     private:
-        int order;
-        
-    class page{
-        public:
-            page();
-            virtual ~page(){}
-            int capacity(){return key.size();}
-            
-        private:
-            vector<int> key;
-    };
+        int page_size; // 兩倍的order
 
-    class index_page : public page
-    {
-        public:
-            index_page(Btree btree);
+        // ------------------------------------------------------ //
+        // - - - - - - - - - - - - 節點 - - - - - - - - - - - - - //
+        // ------------------------------------------------------ //
+        class page{
+            public:
+                page();
+                virtual ~page(){}
 
-        private:
-            page **ptr;
-    };
+                int capacity(){return key.size();}
+                set_type(string type){page_type = type;}
+                string get_type(){return page_type;}
+                push(int value);
+                
+            private:
+                string page_type; 
+                vector<int> key;
+        };
 
-    class leaf_page : public page
-    {
-        public:
-            leaf_page(Btree btree);
+        // --------------------------------------------------------- //
+        // - - - - - - - - - - - - - 路標節點 - - - - - - - - - - - - //
+        // --------------------------------------------------------- //
+        class index_page : public page
+        {
+            public:
+                index_page(Btree btree);
 
-        private:
-            leaf_page *pre_page;
-            leaf_page *next_page;
-    };
+            private:
+                page **ptr;
+        };
 
-    page *root;
+
+        // --------------------------------------------------------- //
+        // - - - - - - - - - - - - - 資料節點 - - - - - - - - - - - - //
+        // --------------------------------------------------------- //
+        class leaf_page : public page
+        {
+            public:
+                leaf_page(Btree btree);
+
+            private:
+                leaf_page *pre_page;
+                leaf_page *next_page;
+        };
+
+
+        page *root;
 };
 
+
+
+
+// ################################################# //
+    // ---------------- 主要程式 ---------------- //
+// ################################################# //
 int main()
 {
     cout << "Hello!" << endl ;
-    Btree tree(5);
+    Btree tree(2);
+    tree.Insert(5);
+    tree.Insert(3);
+    tree.Insert(9);
+    tree.Insert(8);
+    tree.Insert(1);
     return 0;
 }
 
 
+// ################################################# //
+    // ---------------- 函式定義 ---------------- //
+// ################################################# //
 
-// ################ 函式定義 ################ //
-
-Btree::Btree(int input_order){
-    order = input_order;
-    root = new leaf_page(*this);
-    leaf_page *leaf = dynamic_cast<leaf_page*>(root);
+Btree::Btree(int input_order)
+{
+    page_size = input_order * 2;
+    leaf_page *leaf = new leaf_page(*this);
+    root = leaf;
+    //leaf_page *leaf = dynamic_cast<leaf_page*>(root);
 
 }
+
+Btree::Insert(int value)
+{
+    cout << "Try to Insert value: " << value << endl;
+
+    page *temp = root;
+
+    if (temp->get_type() == "leaf"){ // 確認為資料節點
+        
+        temp->push(value);
+        if (temp->capacity() > page_size){ // 檢查是否塞得進去
+            cout << "overflow ! current size: " << temp->capacity() << endl;
+        }
+
+    }
+
+
+
+}
+
 
 
 Btree::page::page(){
 
 }
 
-
-Btree::index_page::index_page(Btree btree){
-    ptr = new page*[btree.order + 1];
-    cout << "Create a Index Page with key size "<< btree.order << endl;
+Btree::page::push(int value)
+{
+    key.push_back(value);
+    sort(key.begin(), key.end());
+    for (int i = 0; i < key.size(); i++) {
+        cout << key[i] << " ";
+    }
+    cout << endl;
 }
 
 
-Btree::leaf_page::leaf_page(Btree btree){
-    cout << "Create a Leaf Page with key size "<< btree.order << endl;
+
+Btree::index_page::index_page(Btree btree)
+{
+    ptr = new page*[btree.page_size*2 + 1];
+    set_type("index");
+    cout << "Create a Index Page with key size "<< btree.page_size << endl;
+}
+
+
+Btree::leaf_page::leaf_page(Btree btree)
+{
+    set_type("leaf");
+    cout << "Create a Leaf Page with key size "<< btree.page_size << endl;
 }
